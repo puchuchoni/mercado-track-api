a523cae451583c6247a712734ab5ba461f233a19
Nicolás Jeremías <jeremias.nicolas@gmail.com>
2019-05-21 00:17:22 -0300

    feat(category routes): add the article count to the category item on the response (#67) (#68)
    every time we request for the category list to the server, it'll include the category count it has.
    The response will be cached for 5 hours

--- src/routes/category/category.route.ts
+++ src/routes/category/category.route.ts
@@ -1,20 +1,43 @@
 import express, { Response, Request } from 'express';
-import { Category } from '../../models';
+import { Category, Article } from '../../models';
+import { Cache } from 'memory-cache';
+import { ICategory } from '../../models/category/category.interface';
+import { Status } from '../article/article.constants';
+
+const cache = new Cache();
+const cacheTime = 5 * 60 * 60 * 1000; // 5 hrs
+const CATEGORY_KEY = 'CATEGORIES';
 
 export const categoryRouter = express.Router();
 
 categoryRouter.route('/')
   .get(getCategories);
 
+interface ICategoryItem extends ICategory {
+  articleCount: number;
+}
+
 async function getCategories(_req: Request, res: Response) {
+  const cachedCategories = cache.get(CATEGORY_KEY);
+  if (cachedCategories) {
+    return res.send({ categories: cachedCategories });
+  }
+
   try {
-    const categories = await Category.find({});
-    res.send({
-      categories,
-      count: categories.length,
-    });
+    const dbCategories = await Category.find({}).lean();
+    const categories: ICategoryItem[] = [];
+    const documentCountPromises = dbCategories.map(
+      dbCategory => Article.find({ category_id: dbCategory._id }).countDocuments(),
+    );
+    const documentCounts = await Promise.all(documentCountPromises);
+    for (let i = 0; i < documentCounts.length; i++) {
+      const dbCategory = dbCategories[i];
+      const articleCount = documentCounts[i];
+      categories.push({ ...dbCategory, articleCount });
+    }
+    cache.put(CATEGORY_KEY, categories, cacheTime);
+    res.send({ categories });
   } catch (error) {
-    // TODO: use variable instead of hardcoded status
-    res.status(500).send({ error });
+    res.status(Status.Error).send({ error });
   }
 }
