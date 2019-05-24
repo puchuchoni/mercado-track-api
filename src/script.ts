62e3e8a9cac058c12b16b633d15df45f68544960
GMaiolo <guille.maiolo@gmail.com>
2019-05-24 00:45:16 -0300

    no se que pasa con los logs

--- src/script.ts
+++ src/script.ts
@@ -1,12 +1,12 @@
 import { MLService } from './services/ml.service';
 import { Article } from './models/article/article.model';
-import { logger } from './shared';
 
+// tslint:disable
 export const addCategoryToArticlesWithNoCategory = async () => {
   const limit = 50;
   const noCategoryQuery = { category_id: { $exists: false } };
   const total = await Article.countDocuments(noCategoryQuery);
-  logger.log({ total });
+  console.log({ total });
   let articles = await Article.find(noCategoryQuery, null, { limit });
   let skip = 0;
   while (articles.length) {
@@ -20,18 +20,18 @@ export const addCategoryToArticlesWithNoCategory = async () => {
         await article.save();
         return ':)';
       } catch (err) {
-        // logger.log('error', err._message);
+        // console.log('error', err._message);
         return ':(';
       }
     });
     try {
       await Promise.all(promises);
-      logger.log(`${skip} / ${total} - ${+(skip * 100 / total).toFixed(3)} - e.g. ${articles[0].id}`);
+      console.log(`${skip} / ${total} - ${+(skip * 100 / total).toFixed(3)} - e.g. ${articles[0].id}`);
       articles = await Article.find(noCategoryQuery, null, { skip, limit });
     } catch (err) {
-      logger.log('error2', err);
+      console.log('error2', err);
       articles = [];
     }
   }
-  logger.log('finished');
+  console.log('finished');
 };
