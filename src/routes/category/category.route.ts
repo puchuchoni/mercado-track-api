aea5a073d35fd88f78e0c4c74949b5d3471d2605
GMaiolo <guille.maiolo@gmail.com>
2019-05-25 02:19:35 -0300

    feat(cache service): added cache service and implemented categories cache with rehydration

--- src/routes/category/category.route.ts
+++ src/routes/category/category.route.ts
@@ -1,11 +1,7 @@
 import express, { Response, Request } from 'express';
-import { Cache } from 'memory-cache';
+import { CacheService } from '../../services';
 import { Status } from '../article/article.constants';
 import { CategoryRouteService } from './category.route.service';
-
-const cache = new Cache();
-const cacheTime = 24 * 60 * 60 * 1000; // 24 hrs
-const CATEGORY_KEY = 'CATEGORIES';
 
 export const categoryRouter = express.Router();
 
@@ -13,13 +9,11 @@ categoryRouter.route('/')
   .get(getCategories);
 
 async function getCategories(_req: Request, res: Response) {
-  const cachedCategories = cache.get(CATEGORY_KEY);
-  if (cachedCategories) {
-    return res.send({ categories: cachedCategories });
+  if (CacheService.categories) {
+    return res.send({ categories: CacheService.categories });
   }
   try {
     const categories = await CategoryRouteService.getCategoriesAggregated();
-    cache.put(CATEGORY_KEY, categories, cacheTime);
     res.send({ categories });
   } catch (error) {
     res.status(Status.Error).send({ error });
